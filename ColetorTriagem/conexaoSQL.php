89520e6ec8e4709340c1e9b1f809f1a932938a2c
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2025-06-04 11:32:21 -0300

    04-06

--- ColetorTriagem/conexaoSQL.php
+++ ColetorTriagem/conexaoSQL.php
@@ -1,22 +1,12 @@
 ﻿<?php
-  $serverName = '192.168.254.197';
-  $connectionInfo = array("Database"=>"POSITIVA", "UID"=>"positivaphp", "PWD"=>"databit@#2023", "CharacterSet"=>"UTF-8");
+ include_once("config.php");
+
+  $serverName = "$server";
+  $connectionInfo = array("Database"=>"$base", "UID"=>"$usuarioBanco", "PWD"=>"$SenhaBanco", "CharacterSet"=>"UTF-8");
   $conn = sqlsrv_connect($serverName, $connectionInfo);
-  
   if($conn){
     echo "";
   }else{
     echo "falha na conex�o";
     die( print_r(sqlsrv_errors(), true));
   }
-  
-  
-  function getConnection() {
-    $serverName = '192.168.254.197';
-    $connectionInfo = array("Database"=>"POSITIVA", "UID"=>"positivaphp", "PWD"=>"databit@#2023", "CharacterSet"=>"UTF-8");
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-    return($conn);
-  }
-
-
-?> 
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/* Variaveis a serem alteradas, mantenha o padrão de preenchimento */
+
+$server = 'localhost';
+$base = 'POSITIVA';
+$usuarioBanco = 'SA';
+$SenhaBanco = 'databit@2022';
+
+
